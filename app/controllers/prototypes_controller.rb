49ca0002840a06e779011249c3dca3175647de66
noche6 <104665556+noche6@users.noreply.github.com>
2022-05-17 18:09:48 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,13 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
-  before_action :set_prototype, only: [:edit, :show, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :new_prototype, only: :new
+  before_action :set_prototype, only: [:edit, :show, :destroy, :update]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   def index 
     @prototype = Prototype.all
   end
   
   def new
-    @prototype = Prototype.new
   end
   
   def create
@@ -28,16 +28,14 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototypes_params)
-      redirect_to prototype_path(prototype.id)
+    if @prototype.update(prototypes_params)
+      redirect_to prototype_path(@prototype.id)
     else
       render :edit
     end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
@@ -46,10 +44,16 @@ class PrototypesController < ApplicationController
   def prototypes_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
   end
+ 
+  def new_prototype
+    @prototype = Prototype.new
+  end
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
   end
+
+ 
 
   def move_to_index
     unless @prototype.user.id == current_user.id
